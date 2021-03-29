f918746e6ccb4ec3ff2f7949e7b37711b7b619b6
Kaalaayaa <kalaya.chatelain@yahoo.fr>
2021-03-29 13:54:48 +0200

    done

---
+++ javascript.js
@@ -0,0 +1,22 @@
+console.log("this is an exercise in console logging");
+
+
+const message = "This is the value";
+console.log(message)
+
+
+const firstName = "Kalaya";
+const lastName = "Chatelain";
+console.log(firstName, lastName)
+
+const first_name = "John";
+const last_name = "Smith"
+const city = "Berlin";
+const job = "teacher";
+const age = "43 year old";
+console.log(first_name, last_name, city, job, age)
+
+
+console.log(first_name, last_name +" is a " + age, job + " living in " + city)
+
+console.log("%s %s is a %s %s living in %s", first_name, last_name, age, job, city)
